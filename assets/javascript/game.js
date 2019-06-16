50dd50d68f5eb53df1e37d54ac014b80cee1fb47
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-16 09:47:18 -0400

    created variables for wins, losses, guesses remaining, letters that were guessed, and an array of letters for the computer to choose at random

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//creating variables for wins, losses, guesses left, and what guesses the user has already made
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var lettersGuessed = "";
+
+
+//creating array for the letters that the computer can choose from
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                "W", "X", "Y", "Z"];
